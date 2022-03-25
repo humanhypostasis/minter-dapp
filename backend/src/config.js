802ee8c5d21eebd72fe3d2399dad64186d9c4f5a
Den <denys.birak.pz.2020@lpnu.ua>
2022-03-25 16:01:36 +0200

    update 1.1

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x6ABd76d7BEA4c7a1D0580EE0A994e56141C051c1"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x6ABd76d7BEA4c7a1D0580EE0A994e56141C051c1"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
